d4d991c2317b13d16bfa8e38f622a89609c6b988
琴梨梨 <yeyaohua2010@163.com>
2021-07-03 14:13:47 +0800

    Update chaoxing.js

--- OnlineMarket/ChaoXing/chaoxing.js
+++ OnlineMarket/ChaoXing/chaoxing.js
@@ -1,8 +1,8 @@
 window.removeEventListener('error',windowErrorHandler);
 Kiyomi.jsSetUA("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
-alert("当前版本1.0.1");
+alert("当前版本1.0.2");
 var injectJS=Base64.decode("amF2YXNjcmlwdDppZihkb2N1bWVudC5sb2NhdGlvbi5wYXRobmFtZS5pbmRleE9mKCJzdHVkZW50c3R1ZHkiKT4xKXt2YXIgZnJhbWVBcnJheT1kb2N1bWVudC5nZXRFbGVtZW50c0J5VGFnTmFtZSgiaWZyYW1lIilbMF0uY29udGVudFdpbmRvdy5kb2N1bWVudC5nZXRFbGVtZW50c0J5VGFnTmFtZSgiaWZyYW1lIik7Zm9yKGk9MDtmcmFtZUFycmF5W2ldO2krKyl7dmFyIHBsYXk9ZnJhbWVBcnJheVtpXS5jb250ZW50V2luZG93LkhUTUxNZWRpYUVsZW1lbnQucHJvdG90eXBlLnBsYXk7ZnJhbWVBcnJheVtpXS5jb250ZW50V2luZG93LkhUTUxNZWRpYUVsZW1lbnQucHJvdG90eXBlLnBsYXk9ZnVuY3Rpb24oKXt0aGlzLnBsYXliYWNrUmF0ZT0xNixwbGF5LmNhbGwodGhpcyl9fX0=");
-Kiyomi.jsInjectPageFinished(injectJS);
+Kiyomi.jsInjectButton2(injectJS,"https://cdn.jsdelivr.net/gh/qinlili23333/KiyomiMethanol@main/OnlineMarket/ChaoXing/baseline_speed_black_48dp.png");
 var pipJS=Base64.decode("amF2YXNjcmlwdDpLaXlvbWkuanNFbnRlclBJUCgyLDEpOw==");
 Kiyomi.jsInjectButton1(pipJS,"https://cdn.jsdelivr.net/gh/qinlili23333/KiyomiMethanol@main/OnlineMarket/ChaoXing/baseline_close_fullscreen_black_48dp.png");
 document.location.href="https://i.mooc.chaoxing.com";
